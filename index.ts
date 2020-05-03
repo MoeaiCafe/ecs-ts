c679e0a427b773fd6c4decbd2a76ff4ec477a9b0
Rossfuin <darksheepgod@outlook.com>
2020-05-04 07:04:56 +0800

    add some test code

--- index.ts
+++
@@ -1,7 +0,0 @@
-export { Component } from './component/Component';
-export { Context } from './context/Context';
-export { ContextImpl } from './context/ContextImpl';
-export { Entity } from './entity/Entity';
-export { allOf, anyOf, noneOf } from './matcher/Matchers';
-export { System } from './systems/System';
-export { Systems } from './systems/Systems';
